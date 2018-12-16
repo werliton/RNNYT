f0000cf880e98fda24aa3f20c289b258721164ee
werliton <werlitoncarlos@gmail.com>
2018-12-16 10:54:39 -0300

    Creating SmallText component

---
+++ src/components/SmallText.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet, Text } from 'react-native'
+import AppText from './AppText'
+
+const SmallText = ({ children, style, ...rest }) => {
+    <AppText style={[styles.small, style]} {...rest}>
+        {children}
+    </AppText>
+}
+
+SmallText.propTypes = {
+    children: PropTypes.node,
+    style: Text.propTypes.style
+}
+
+const styles = StyleSheet.create({
+    small:{
+        fontSize: 11
+    }
+})
+
+export default SmallText
