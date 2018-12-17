b52630b940d304d60228eb3efb34f06499331592
werliton <werlitoncarlos@gmail.com>
2018-12-16 21:07:00 -0300

    Using reshapenewdata in NewsFeedContainer

--- src/containers/NewsFeedContainer.js
+++ src/containers/NewsFeedContainer.js
@@ -2,9 +2,11 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { loadNews } from '../actions/newsActions'
 import NewsFeed from '../components/NewsFeed'
+import { reshapeNewsData } from '../util/dataTransformations'
 
+// Stop in page 228
 const mapStateToProps = state => ({
-    news: state.news
+    news: reshapeNewsData(state.news)
 })
 
 const mapDispatchToProps = dispatch => (
